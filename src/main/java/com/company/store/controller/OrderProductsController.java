704dd527097368f1c14b41a8ced4322bbcf54a1e
paulhoriachiy <pashenkagor@gmail.com>
2018-05-22 17:39:08 +0300

    add  OrderProductController

--- src/main/java/com/company/store/controller/OrderProductsController.java
+++ src/main/java/com/company/store/controller/OrderProductsController.java
@@ -17,25 +17,26 @@ public class OrderProductsController {
     private OrderProductsDAOImpl orderProductsDAO;
 
     @RequestMapping(value = "/OrderProductsById/{order_id}")
-    public ModelAndView getOrderProductsById(@PathVariable int order_id){
+    public ModelAndView getOrderProductsById(@PathVariable int order_id) {
         Collection<OrderProduct> orderProducts = orderProductsDAO.getOrderProductsById(order_id);
         return new ModelAndView("orderProductById", "orderProducts", orderProducts);
     }
 
     @RequestMapping(value = "/removeAllProductsFromOrder/{order_id}")
-    public ModelAndView removeAllProductsFromOrder(@PathVariable int order_id){
+    public ModelAndView removeAllProductsFromOrder(@PathVariable int order_id) {
         orderProductsDAO.removeAllProductsFromOrder(order_id);
         return new ModelAndView("removeAllProducts");
     }
 
     @RequestMapping(value = "/saveProductToOrder/{product}{order}{amount}{price}{isUpdate}")
-    public ModelAndView saveProductToOrder(@PathVariable Product product, Order order, int amount, float price, boolean isUpdate){
-        orderProductsDAO.saveProductToOrder(product,order, amount,price,isUpdate);
+    public ModelAndView saveProductToOrder(@PathVariable Product product, Order order, int amount, float price, boolean isUpdate) {
+        orderProductsDAO.saveProductToOrder(product, order, amount, price, isUpdate);
         return new ModelAndView("/saveProductToOrder");
     }
-        @RequestMapping(value = "/removeProductFromOrder{product_id}{order_id}")
-            public ModelAndView remove(@PathVariable int product_id, int order_id){
-                orderProductsDAO.removeProductFromOrder(product_id,order_id);
-                return new ModelAndView("/removeProductFromOrder");
-        }
+
+    @RequestMapping(value = "/removeProductFromOrder{product_id}{order_id}")
+    public ModelAndView remove(@PathVariable int product_id, int order_id) {
+        orderProductsDAO.removeProductFromOrder(product_id, order_id);
+        return new ModelAndView("/removeProductFromOrder");
+    }
 }
