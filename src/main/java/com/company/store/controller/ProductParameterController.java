b06ed061d3351c07b9b405713e8f09eaf627f56e
paulhoriachiy <pashenkagor@gmail.com>
2018-05-25 16:54:16 +0300

    update PaymentController1

--- src/main/java/com/company/store/controller/ProductParameterController.java
+++ src/main/java/com/company/store/controller/ProductParameterController.java
@@ -1,10 +1,8 @@
 package com.company.store.controller;
 
-import com.company.store.model.entities.Product;
 import com.company.store.model.entities.ProductParameter;
 import com.company.store.model.impls.ProductParameterDAOImpl;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
