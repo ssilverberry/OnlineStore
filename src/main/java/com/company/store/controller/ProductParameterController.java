f565fed79bec6da1c4d83f4410c0a95841222949
paulhoriachiy <pashenkagor@gmail.com>
2018-05-25 15:51:19 +0300

    update ProductParemetrControllervoller

--- src/main/java/com/company/store/controller/ProductParameterController.java
+++ src/main/java/com/company/store/controller/ProductParameterController.java
@@ -6,6 +6,7 @@ import com.company.store.model.impls.ProductParameterDAOImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Collection;
@@ -19,21 +20,27 @@ public class ProductParameterController {
         this.productParameterDAO = productParameterDAO;
     }
 
-    @RequestMapping(value = "parambyproductandattrids/{id}/attr/{id}")
-    public ModelAndView getParamByProductAndAttrIds(@PathVariable("id") String id) {
-        ProductParameter productParameter = productParameterDAO.getParamByProductAndAttrIds(Integer.parseInt(id), Integer.parseInt(id));
-        return new ModelAndView("productparamandattribs", "productParameter", productParameter);
+    @RequestMapping(value = "parambyproductandattrids")
+    public ModelAndView getParamByProductAndAttrIds(@RequestParam(value = "id") String id,
+                                                    @RequestParam (value = "attr_id") String attr_id) {
+        ProductParameter productParameter =
+                productParameterDAO.getParamByProductAndAttrIds(Integer.parseInt(id), Integer.parseInt(id));
+        return new ModelAndView("productparamandattribs",
+                "productParameter", productParameter);
     }
 
-    @RequestMapping(value = "saveparameter")
+    @RequestMapping(value = "saveparameter") // not have solution fo this boolean variable & object
     public ModelAndView saveParameter(ProductParameter productParam, boolean isUpdate) {
         productParameterDAO.saveParameter(productParam, isUpdate);
         return new ModelAndView("saveupdate");
     }
 
-    @RequestMapping(value = "removeparameterbyproductid/{id}")
-    public ModelAndView removeParameterByProductId(@PathVariable("id") String id) {
-        productParameterDAO.removeParameterByProductId(Integer.parseInt(id));
+    @RequestMapping(value = "removeparameterbyproductid")
+    public ModelAndView removeParameterByProductId(@RequestParam("id") int id) {
+        Boolean rem = productParameterDAO.removeParameterByProductId(id);
+        if(rem == true){
         return new ModelAndView("removeparameterbyproductid");
+    }else
+        return new ModelAndView("notRemove");
     }
 }
