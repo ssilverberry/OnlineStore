cbb530faa13496d4589de61c9578dd8f31f61355
paulhoriachiy <pashenkagor@gmail.com>
2018-05-26 11:07:56 +0300

    update OrderProductController

--- src/main/java/com/company/store/controller/OrderProductsController.java
+++ src/main/java/com/company/store/controller/OrderProductsController.java
@@ -8,8 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.sql.Timestamp;
 import java.util.Collection;
 
 @Controller
@@ -21,27 +23,41 @@ public class OrderProductsController {
         this.orderProductsDAO = orderProductsDAO;
     }
 
-    @RequestMapping(value = "OrderProductsById/{id}")
-    public ModelAndView getOrderProductsById(@PathVariable("id") String id) {
-        Collection<OrderProduct> orderProducts = orderProductsDAO.getOrderProductsById(Integer.parseInt(id));
+    @RequestMapping(value = "OrderProductsById")
+    public ModelAndView getOrderProductsById(@RequestParam(value = "id") int id) {
+        Collection<OrderProduct> orderProducts = orderProductsDAO.getOrderProductsById(id);
         return new ModelAndView("orderProductById", "orderProducts", orderProducts);
     }
 
-    @RequestMapping(value = "removeAllProductsFromOrder/{id}")
-    public ModelAndView removeAllProductsFromOrder(@PathVariable("id") String id) {
-        orderProductsDAO.removeAllProductsFromOrder(Integer.parseInt(id));
+    @RequestMapping(value = "removeAllProductsFromOrder")
+    public ModelAndView removeAllProductsFromOrder(@RequestParam("id") int id) {
+        orderProductsDAO.removeAllProductsFromOrder(id);
         return new ModelAndView("removeAllProducts");
     }
 
     @RequestMapping(value = "saveProductToOrder/")
-    public ModelAndView saveProductToOrder(@PathVariable Product product, Order order, int amount, float price, boolean isUpdate) {
+    public ModelAndView saveProductToOrder(@RequestParam(value = "id") int product,
+                                           @RequestParam(value = "order_id") int order_id,
+                                           @RequestParam(value = "date") Timestamp date,
+                                           @RequestParam(value = "user_id") int user_id,
+                                           @RequestParam(value = "payment_id") int payment_id,
+                                           @RequestParam(value = "delivery_id") int delivery_id,
+                                           @RequestParam(value = "amount") int amount,
+                                           @RequestParam(value = "price") float price,
+                                           @RequestParam(value = "isUpdate") boolean isUpdate) {
+        Order order = new Order();
+        order.setId(order_id);
+        order.setDate(date);
+        order.setUserId(user_id);
+        order.setDeliveryId(delivery_id);
+        order.setPaymentId(payment_id);
         orderProductsDAO.saveProductToOrder(product, order, amount, price, isUpdate);
         return new ModelAndView("saveProductToOrder");
     }
 
-    @RequestMapping(value = "removeProductFromOrder/{id}/order/{id}")
-    public ModelAndView remove(@PathVariable("id") String id) {
-        orderProductsDAO.removeProductFromOrder(Integer.parseInt(id), Integer.parseInt(id));
+    @RequestMapping(value = "removeProductFromOrderorder")
+    public ModelAndView remove(@RequestParam(value = "id") int id, @RequestParam(value = "order_id") int order_id) {
+        orderProductsDAO.removeProductFromOrder(id, order_id);
         return new ModelAndView("removeProductFromOrder");
     }
 }
