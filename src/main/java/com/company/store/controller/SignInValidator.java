5616c5ad8f5e58332bc043aae4d9b1f9e8a4e734
Paul <pashenkagor@gmail.com>
2018-07-02 01:50:19 +0300

    commit before rebasing into dev branch.

--- src/main/java/com/company/store/controller/SignInValidator.java
+++ src/main/java/com/company/store/controller/SignInValidator.java
@@ -3,7 +3,6 @@ package com.company.store.controller;
 
 import com.company.store.model.entities.User;
 import com.company.store.model.impls.UserDAOImpl;
-import org.apache.commons.validator.routines.EmailValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
