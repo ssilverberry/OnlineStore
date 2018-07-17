36cb663f0ca455ea7336cbccc1443c5aa7617812
YuraLampak <pekines333@gmail.com>
2018-07-17 15:08:49 +0300

    added validation common values of fields in form for categories creating

--- src/main/java/com/company/store/model/validators/CreateCategoryFormValidator.java
+++ src/main/java/com/company/store/model/validators/CreateCategoryFormValidator.java
@@ -10,7 +10,9 @@ import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
 import org.springframework.validation.Validator;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 @Component
 public class CreateCategoryFormValidator implements Validator {
@@ -38,28 +40,70 @@ public class CreateCategoryFormValidator implements Validator {
         Collection<Product> existCategories = productDAO.getCategories();
         existCategories.addAll(productDAO.getSubcategories());
 
-        category.setName(category.getName().trim());
+        trimNames(category);
 
-        for (Product prod: existCategories) {
-            if (category.getName().equalsIgnoreCase(prod.getName())){
-                errors.rejectValue("name", "Exist.category.params.value");
+        checkOnCommonCategories(category, errors);
+
+        checkOnExistCategories(category, errors, existCategories);
+
+    }
+
+    private void checkOnCommonCategories(Product category, Errors errors){
+        List<Integer> commonIndexes = new ArrayList<>();
+        boolean flag = false;
+        for (int i = 0; i < category.getParams().size(); i++) {
+            ProductParameter subcategory = category.getParams().get(i);
+            if (subcategory == null || subcategory.getValue().equals("")){
+                errors.rejectValue("params[" + i + "].value", "NotEmpty.category.params.value");
+            } else if (category.getName().equalsIgnoreCase(subcategory.getValue())){
+                if (!flag) {
+                    errors.rejectValue("name", "Identity.category.name");
+                    flag = true;
+                }
+                if (!commonIndexes.contains(i)) {
+                    commonIndexes.add(i);
+                    errors.rejectValue("params[" + i + "].value", "Identity.category.params.value");
+                }
+            }
+            else {
+                for (int j = 0; j < category.getParams().size(); j++){
+                    if (i != j){
+                        ProductParameter compareSub = category.getParams().get(j);
+                        if (subcategory.getValue().equalsIgnoreCase(compareSub.getValue())){
+                            if (!commonIndexes.contains(j)) {
+                                commonIndexes.add(j);
+                                errors.rejectValue("params[" + j + "].value", "Identity.category.params.value");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private void checkOnExistCategories(Product category, Errors errors, Collection<Product> existCategories){
+
+        for (int i = 0; i < category.getParams().size(); i++) {
+            ProductParameter subcateg = category.getParams().get(i);
+            for (Product prod : existCategories) {
+                if (category.getName().equalsIgnoreCase(prod.getName())){
+                    errors.rejectValue("name", "Exist.category.params.value");
+                }
+                if (subcateg.getValue().equalsIgnoreCase(prod.getName())) {
+                    errors.rejectValue("params[" + i + "].value", "Exist.category.params.value");
+                }
             }
         }
 
+    }
+
+    private void trimNames(Product category){
+        category.setName(category.getName().trim());
         for (int i = 0; i < category.getParams().size(); i++) {
             ProductParameter subcateg = category.getParams().get(i);
             if (subcateg != null){
                 subcateg.setValue(subcateg.getValue().trim());
                 category.getParams().set(i, subcateg);
-            }
-            if (subcateg == null || subcateg.getValue().equals("")){
-                errors.rejectValue("params[" + i + "].value", "NotEmpty.category.params.value");
-            } else {
-                for (Product prod : existCategories) {
-                    if (subcateg.getValue().equalsIgnoreCase(prod.getName())) {
-                        errors.rejectValue("params[" + i + "].value", "Exist.category.params.value");
-                    }
-                }
             }
         }
     }
