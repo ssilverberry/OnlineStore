cc700d1a4e7d94a5c72f3da4a0793de5c30348e0
YuraLampak <pekines333@gmail.com>
2018-07-20 15:30:41 +0300

    updated validator for create/update product

--- src/main/java/com/company/store/model/validators/ProductFormValidator.java
+++ src/main/java/com/company/store/model/validators/ProductFormValidator.java
@@ -9,8 +9,11 @@ import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Component
-public class UpdateProductFormValidator implements Validator {
+public class ProductFormValidator implements Validator {
 
     private ProductDAO productDAO;
 
@@ -39,13 +42,16 @@ public class UpdateProductFormValidator implements Validator {
         if (productName.equals("")){
             errors.rejectValue("name", "NotEmpty.product.name");
         }
-        /*Product prodDB = productDAO.getProductByName(productName);
-        if (prodDB != null){
-            errors.rejectValue("name", "Exist.product.name");
-        }*/
+        if (product.getId() == 0){
+            Product prodDB = productDAO.getProductByName(productName);
+            if (prodDB != null){
+                errors.rejectValue("name", "Exist.product.name");
+            }
+        }
     }
 
     private void checkParameters(Product product, Errors errors){
+        List<Integer> commonIndexes = new ArrayList<>();
         for (int i = 0; i < product.getParams().size(); i++) {
             ProductParameter parameter = product.getParams().get(i);
             if (parameter != null){
@@ -54,6 +60,18 @@ public class UpdateProductFormValidator implements Validator {
             }
             if (parameter == null || parameter.getValue().equalsIgnoreCase("")){
                 errors.rejectValue("params[" + i + "].value", "NotEmpty.product.params.value");
+            } else {
+                for (int j = 0; j < product.getParams().size(); j++){
+                    if (i != j){
+                        ProductParameter paramToCompare = product.getParams().get(j);
+                        if (parameter.getValue().equalsIgnoreCase(paramToCompare.getValue())){
+                            if (!commonIndexes.contains(j)) {
+                                commonIndexes.add(j);
+                                errors.rejectValue("params[" + j + "].value", "Identity.product.params.value");
+                            }
+                        }
+                    }
+                }
             }
         }
     }
